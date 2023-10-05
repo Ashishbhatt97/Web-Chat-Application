e8663a0bad9a36e5b13900b00de80a6f22442745
Ashish bhatt <117436056+Ashish0197@users.noreply.github.com>
2023-10-05 19:46:26 +0530

    Implemented Facebook Page

--- firebase/firbase.js
+++ firebase/firbase.js
@@ -1,37 +1,16 @@
-// import { initializeApp } from "firebase/app";
-// import {getAuth} from "firebase/auth"
-// import {getStorage} from "firebase/storage"
-// import {getFirestore} from "firebase/firestore"
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyC_DI1CWRfTE1WhFmVc_ewe-M0j8M0ol4A",
-//   authDomain: "book-b3a6f.firebaseapp.com",
-//   projectId: "book-b3a6f",
-//   storageBucket: "book-b3a6f.appspot.com",
-//   messagingSenderId: "691860095734",
-//   appId: "1:691860095734:web:6dd477346b1234dac1e2d7"
-// };
-// Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-
-// export const auth = getAuth(app);
-// export const firestore = getFirestore(app);
-// export const storage = getStorage(app);
-
-
-
 import { initializeApp } from "firebase/app";
 import {getAuth} from "firebase/auth"
 import {getStorage} from "firebase/storage"
 import {getFirestore} from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBuCjHYysdf-aoi2uFnJKuxWdAkKikK74c",
-  authDomain: "chit-chat-403fc.firebaseapp.com",
-  projectId: "chit-chat-403fc",
-  storageBucket: "chit-chat-403fc.appspot.com",
-  messagingSenderId: "753747236052",
-  appId: "1:753747236052:web:bbb9553d179e11517dd741"
+  apiKey: "AIzaSyBLfc5LzXmSKGty7CIlNRrk5vpXgmceLew",
+  authDomain: "chatting-e3f82.firebaseapp.com",
+  databaseURL: "https://chatting-e3f82-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "chatting-e3f82",
+  storageBucket: "chatting-e3f82.appspot.com",
+  messagingSenderId: "246432720523",
+  appId: "1:246432720523:web:098781dbbd56655cec568e"
 };
 
 // Initialize Firebase
